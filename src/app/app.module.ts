62ed9039b15d5dab2eed18d19dccb8f0539a8971
Davi <davimss@gmail.com>
2019-08-11 19:53:38 -0300

    Aula 10.5 concluída

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 // Imports adicionais
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
