3c3d0f35bcf855fe88918675b02fe863c2dd9906
Davi <davimss@gmail.com>
2019-11-07 18:24:49 -0300

    Aula 13.9 concluída

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-message',
+  template: `
+  <div *ngIf="temErro()" class="ui-messages ui-messages-error">
+    {{ text }}
+  </div>  
+  `,
+  styles: [`
+  .ui-messages.ui-messages-error {    
+    background-color: #ff0000;
+    color: #ffffff;
+    border-radius: 4px;
+    padding: 3px;
+    margin: 2px 0;
+    font-weight: bold;
+}`]
+})
+export class MessageComponent {
+
+  @Input() error: string;
+  @Input() control: FormControl;
+  @Input() text: string;
+
+  temErro(): boolean {
+    return this.control.hasError(this.error) && this.control.dirty;
+  }
+
+
+}
